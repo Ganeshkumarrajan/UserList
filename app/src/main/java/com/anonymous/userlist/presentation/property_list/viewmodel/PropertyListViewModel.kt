93b15736616b254a112998f77c2199fca0da1358
ganeshkumarraja <Ganeshapikey@gmail.com>
2023-05-01 19:05:10 +0200

    add user interface to property list

--- app/src/main/java/com/anonymous/userlist/presentation/property_list/viewmodel/PropertyListViewModel.kt
+++ app/src/main/java/com/anonymous/userlist/presentation/property_list/viewmodel/PropertyListViewModel.kt
@@ -1,19 +1,30 @@
-package com.anonymous.userlist.presentation
+package com.anonymous.userlist.presentation.property_list
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.anonymous.userlist.domain.GetPropertyListUseCase
 import com.anonymous.userlist.domain.NetworkResult
 import com.anonymous.userlist.domain.PropertyListDomain
+import com.anonymous.userlist.presentation.UIState
+import com.anonymous.userlist.ui.theme.component.PropertyCardProperties
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PropertyListViewModel @Inject constructor(private val getPropertyListUseCase: GetPropertyListUseCase) :
+class PropertyListViewModel @Inject constructor(
+    private val getPropertyListUseCase:
+    GetPropertyListUseCase,
+    private val mapperDomainToUI: MapperDomainToUI<List<PropertyListDomain>, List<PropertyCardProperties>>
+) :
     ViewModel() {
+    private val _properties =
+        mutableStateOf<UIState<List<PropertyCardProperties>>>(UIState.Loading())
+    val properties = _properties
+
     init {
         getProperties()
     }
@@ -30,10 +41,10 @@ class PropertyListViewModel @Inject constructor(private val getPropertyListUseCa
     private fun parsePropertyListResult(data: NetworkResult<List<PropertyListDomain>>) {
         when (data) {
             is NetworkResult.Success -> {
-                Log.d("d",data.data.toString())
+                properties.value = UIState.Success(mapperDomainToUI.map(data = data.data))
             }
             is NetworkResult.Error -> {
-
+                properties.value = UIState.Error()
             }
         }
     }
